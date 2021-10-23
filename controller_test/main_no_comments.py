5b2ecefdd9718094062033e524e5346d25aba4f1
Jorge <hernandezj0174@gmail.com>
2021-10-23 02:09:10 -0500

    fixed both versions to work

--- controller_test/main_no_comments.py
+++ controller_test/main_no_comments.py
@@ -1,48 +1,61 @@
+import sys
+
 import pygame
+
+from pygame.locals import *
 pygame.init()
-
-gameDisplay = pygame.display.set_mode((800, 800))
-pygame.display.set_caption("Controller Test")
-
-pygame.display.update()
-
-gameExit = False
-
-lead_x = 300
-lead_y = 300
-
-white = (255, 255, 255)
-red = (255, 0, 0)
-
-joysticks = []
+pygame.display.set_caption('game base')
+screen = pygame.display.set_mode((500, 500), 0, 32)
 clock = pygame.time.Clock()
 
-for i in range(pygame.joystick.get_count()):
-    joysticks.append(pygame.joystick.Joystick(i))
-    joysticks[-1].init()
+pygame.joystick.init()
+joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
+for joystick in joysticks:
+    print(joystick.get_name())
 
-while not gameExit:
-    clock.tick(60)
+my_square = pygame.Rect(50, 50, 50, 50)
+my_square_color = 0
+colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+motion = [0, 0]
+
+while True:
+
+    screen.fill((0, 0, 0))
+
+    pygame.draw.rect(screen, colors[my_square_color], my_square)
+    if abs(motion[0]) < 0.1:
+        motion[0] = 0
+    if abs(motion[1]) < 0.1:
+        motion[1] = 0
+    my_square.x += motion[0] * 10
+    my_square.y += motion[1] * 10
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameExit = True
-    
-    if event.type == pygame.JOYAXISMOTION:
-        print(joysticks[0].get_axis(0))
-        if joysticks[0].get_axis(0) >= 0.5:    # Right
-            lead_x += 10
-        if joysticks[0].get_axis(0) <= -0.5:   # Left
-            lead_x -= 10
-        if joysticks[0].get_axis(0) > -0.5 and joysticks[0].get_axis(0) < 0.5:  # Up
-            lead_y -= 10
-        if joysticks[0].get_axis(0) > -0.5 and joysticks[0].get_axis(0) < 0.5:   # Down
-            lead_y += 10
-    
-    gameDisplay.fill(red)
-    pygame.draw.rect(gameDisplay, white, [lead_x, lead_y, 20, 20])
+        if event.type == JOYBUTTONDOWN:
+            print(event)
+            if event.button == 0:
+                my_square_color = (my_square_color + 1) % len(colors)
+        if event.type == JOYBUTTONUP:
+            print(event)
+        if event.type == JOYAXISMOTION:
+            print(event)
+            if event.axis < 2:
+                motion[event.axis] = event.value
+        if event.type == JOYHATMOTION:
+            print(event)
+        if event.type == JOYDEVICEADDED:
+            joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
+            for joystick in joysticks:
+                print(joystick.get_name())
+        if event.type == JOYDEVICEREMOVED:
+            joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
 
     pygame.display.update()
-
-pygame.quit()
-quit()
+    clock.tick(60)
