5b3e79b2aece6c8823461919fd8f64eebf338e50
cameronclay2018 <cameronclay2018@gmail.com>
2021-10-29 18:39:04 -0500

    added backgroud stuff

--- lumberjack_platformer/lumberjack_platformer.py
+++ lumberjack_platformer/lumberjack_platformer.py
@@ -9,7 +9,17 @@ pygame.display.set_caption("Lumberjack Platformer!")
 MAIN_BG = pygame.transform.scale(pygame.image.load(os.path.join("lumberjack_platformer\\assets", "CloudsBack.png")), (WIDTH, HEIGHT))
 BOTTOM_CLOUDS = pygame.transform.scale(pygame.image.load(os.path.join("lumberjack_platformer\\assets", "CloudsFront.png")), (WIDTH, HEIGHT))
 MOUNTAINS = pygame.transform.scale(pygame.image.load(os.path.join("lumberjack_platformer\\assets", "BGBack.png")), (WIDTH, HEIGHT))
+GRASS_TILES = 0
 
 while True:
     SCREEN.blit(MAIN_BG, (0, 0))
+    SCREEN.blit(BOTTOM_CLOUDS, (0, 0))
+    SCREEN.blit(MOUNTAINS, (0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    pygame.display.update()
+    
+pygame.quit()
 
