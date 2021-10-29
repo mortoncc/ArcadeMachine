d674da9e66831708cb921471bfeca75844a0ff59
Jorge <hernandezj0174@gmail.com>
2021-10-29 18:08:39 -0500

    updated assets folder and loaded images in py file

---
+++ lumberjack_platformer/lumberjack_platformer.py
@@ -0,0 +1,15 @@
+import pygame
+import os
+pygame.init()
+
+WIDTH, HEIGHT = 750, 750
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Lumberjack Platformer!")
+
+MAIN_BG = pygame.transform.scale(pygame.image.load(os.path.join("lumberjack_platformer\\assets", "CloudsBack.png")), (WIDTH, HEIGHT))
+BOTTOM_CLOUDS = pygame.transform.scale(pygame.image.load(os.path.join("lumberjack_platformer\\assets", "CloudsFront.png")), (WIDTH, HEIGHT))
+MOUNTAINS = pygame.transform.scale(pygame.image.load(os.path.join("lumberjack_platformer\\assets", "BGBack.png")), (WIDTH, HEIGHT))
+
+while True:
+    SCREEN.blit(MAIN_BG, (0, 0))
+
