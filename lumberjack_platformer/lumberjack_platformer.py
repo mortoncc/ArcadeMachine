9e750114f6105b08205005b2f195ffea4691f728
cameronclay2018 <cameronclay2018@gmail.com>
2021-10-29 18:57:46 -0500

    added images
    added grass tile and player

--- lumberjack_platformer/lumberjack_platformer.py
+++ lumberjack_platformer/lumberjack_platformer.py
@@ -9,12 +9,14 @@ pygame.display.set_caption("Lumberjack Platformer!")
 MAIN_BG = pygame.transform.scale(pygame.image.load(os.path.join("lumberjack_platformer\\assets", "CloudsBack.png")), (WIDTH, HEIGHT))
 BOTTOM_CLOUDS = pygame.transform.scale(pygame.image.load(os.path.join("lumberjack_platformer\\assets", "CloudsFront.png")), (WIDTH, HEIGHT))
 MOUNTAINS = pygame.transform.scale(pygame.image.load(os.path.join("lumberjack_platformer\\assets", "BGBack.png")), (WIDTH, HEIGHT))
-GRASS_TILES = 0
-
+GRASS_TILES = pygame.transform.scale(pygame.image.load(os.path.join("lumberjack_platformer\\assets", "middle_grass_tile.png")), (64, 64))
+PLAYER =  pygame.transform.scale(pygame.image.load(os.path.join("lumberjack_platformer\\assets", "LumberjackMale.png")), (64, 64))
 while True:
     SCREEN.blit(MAIN_BG, (0, 0))
     SCREEN.blit(BOTTOM_CLOUDS, (0, 0))
     SCREEN.blit(MOUNTAINS, (0, 0))
+    SCREEN.blit(GRASS_TILES, (WIDTH/2-32,HEIGHT-64))
+    SCREEN.blit(PLAYER, (WIDTH/2, HEIGHT/2))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
