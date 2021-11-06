abc7a865e0e38ddd62b5b97b7010bc24ce888afc
Jorge <43319415+hernandezj0174@users.noreply.github.com>
2021-11-06 01:01:26 -0500

    Added more code to the tutorial folder

--- platformer_tutorial/level.py
+++ platformer_tutorial/level.py
@@ -2,6 +2,7 @@ import pygame
 from tiles import Tile
 from settings import tile_size
 from player import Player
+from settings import screen_width
 
 class Level:
     def __init__(self, level_data, surface):
@@ -22,12 +23,43 @@ class Level:
                 if cell == 'P':
                     player_sprite = Player((x, y))
                     self.player.add(player_sprite)
+    
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player_x < screen_width / 4 and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > screen_width - (screen_width / 4) and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
+    
+    def horizontal_movement_collision(self):
+        player = self.player.sprite
+        player.rect.x += player.direction.x * player.speed
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.x < 0:
+                    player.rect.left = sprite.rect.right
+                elif player.direction.x > 0:
+                    player.rect.right = sprite.rect.left
+    
+    def vertical_movement_collision(self):
+        player = self.player.sprite
 
     def run(self):
         # level tiles
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+        self.scroll_x
 
         # player
         self.player.update()
+        self.horizontal_movement_collision()
         self.player.draw(self.display_surface)
