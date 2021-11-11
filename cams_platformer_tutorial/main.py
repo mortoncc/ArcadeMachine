47db7c6e47283ad408b569a8c7feafa95cd8a45f
cameronclay2018 <cameronclay2018@gmail.com>
2021-11-10 18:42:16 -0600

    cams_platformer overworld creation

--- cams_platformer_tutorial/main.py
+++ cams_platformer_tutorial/main.py
@@ -6,7 +6,7 @@ from overworld import Overworld
 class Game:
     def __init__(self):
         self.max_level = 3
-        self.overworld = Overworld(0, self.max_level, screen)
+        self.overworld = Overworld(1, self.max_level, screen)
 
     def run(self):
         self.overworld.run()
@@ -15,7 +15,7 @@ class Game:
 pygame.init()
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
-level = Level(level_map, screen)
+#level = Level(level_map, screen)
 game = Game()
 
 while True:
@@ -24,10 +24,10 @@ while True:
             pygame.quit()
             sys.exit()
 
-    #screen.fill('black')
-    screen.blit(level.background, (0, 0))
+    screen.fill('black')
+    #screen.blit(level.background, (0, 0))
     game.run()
-    level.run()
+    #level.run()
 
     pygame.display.update()
     clock.tick(60)
