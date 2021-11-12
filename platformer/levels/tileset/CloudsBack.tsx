625700584eea3c14b9d9710cea0061bf4ac643f4
Sancheza38 <93687081+Sancheza38@users.noreply.github.com>
2021-11-12 01:55:32 -0600

    added platformer folder
    filepaths need to be rewritten to solve "FileNotFoundError".

---
+++ platformer/levels/tileset/CloudsBack.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="CloudsBack" tilewidth="64" tileheight="64" tilecount="40" columns="8">
+ <image source="../level data/CloudsBack.png" width="512" height="320"/>
+</tileset>
