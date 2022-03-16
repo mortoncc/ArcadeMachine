2515518535efa75b9944df3e702c8884857d3ef2
Sancheza38 <93687081+Sancheza38@users.noreply.github.com>
2022-03-16 18:22:47 -0500

    Update level.py

--- platformer/code/level.py
+++ platformer/code/level.py
@@ -5,19 +5,19 @@ from tiles import Tile, StaticTile
 
 class Level:
     def __init__(self,level_data,surface):
-        # general setup
+        #general setup
         self.display_surface = surface
         self.world_shift = -5
 
-        # terrain setup
+        #terrain setup
         terrain_layout = import_csv_layout(level_data['terrain'])
         self.terrain_sprites = self.create_tile_group(terrain_layout,'terrain')
 
-        # tree setup
-        #fordrop_layout = import_csv_layout(level_data['fordrop'])
-        #self.fordrop_sprites = self.create_tile_group(fordrop_layout,'fordrop')
+        #tree setup
+        fordrop_layout = import_csv_layout(level_data['fordrop'])
+        self.fordrop_sprites = self.create_tile_group(fordrop_layout,'fordrop')
 
-        # coins setup
+        #coins setup
         coins_layout = import_csv_layout(level_data['coins'])
         self.coins_sprites = self.create_tile_group(coins_layout,'coins')
 
@@ -35,10 +35,10 @@ class Level:
                         tile_surface = terrain_tile_list[int(val)]
                         sprite = StaticTile(tile_size,x,y,tile_surface)
                     
-   #                 if type == 'fordrop':
-   #                     fordrop_tile_list = import_cut_graphics('../graphics/decorations/Tree.png')
-   #                     tile_surface = fordrop_tile_list[int(val)]
-   #                     sprite = StaticTile(tile_size,x,y,tile_surface)
+                    if type == 'fordrop':
+                        fordrop_tile_list = import_cut_graphics('../graphics/decorations/Tree.png')
+                        tile_surface = fordrop_tile_list[int(val)]
+                        sprite = StaticTile(tile_size,x,y,tile_surface)
                         
                     if type == 'coins':
                         coin_tile_list = import_cut_graphics('../graphics/coins/Pinecone.png')
@@ -56,9 +56,9 @@ class Level:
         self.terrain_sprites.update(self.world_shift)        
         self.terrain_sprites.draw(self.display_surface)
 
-     #   # fordrop
-     #   self.fordrop_sprites.update(self.world_shift)
-     #   self.fordrop_sprites.draw(self.display_surface)
+        # fordrop
+        self.fordrop_sprites.update(self.world_shift)
+        self.fordrop_sprites.draw(self.display_surface)
 
         # coins
         self.coins_sprites.update(self.world_shift)
